a43b0ce529264f7f37ebfcf491399468ba88f704
andresinho20049 <andre.andresinho2009@hotmail.com>
2024-08-26 01:55:26 -0300

    feat: Close mobile menu when clicking outside the menu or clicking on a link

--- src/components/client/navbar/navbar-link.tsx
+++ src/components/client/navbar/navbar-link.tsx
@@ -2,15 +2,27 @@
 
 import Link, { LinkProps } from "next/link";
 import { usePathname } from "next/navigation";
-import { ReactNode } from "react";
+import { ReactNode, useCallback } from "react";
 
 export type NavBarLinkType = LinkProps & {
 	href: string;
 	label: string | ReactNode;
+	callback?: () => void;
 };
 
-export const NavBarLink = ({ href, label, ...props }: NavBarLinkType) => {
+export const NavBarLink = ({
+	href,
+	label,
+	callback,
+	...props
+}: NavBarLinkType) => {
 	const pathname = usePathname();
+
+	const handleClick = useCallback(() => {
+		if (callback) {
+			callback();
+		}
+	}, []);
 
 	return (
 		<Link
@@ -19,6 +31,7 @@ export const NavBarLink = ({ href, label, ...props }: NavBarLinkType) => {
 				href === pathname ? "text-primary dark:text-primary" : ""
 			} md:border-none`}
 			{...props}
+			onClick={handleClick}
 		>
 			{label}
 		</Link>
--- src/components/client/navbar/navbar.tsx
+++ src/components/client/navbar/navbar.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { pages } from "@onion/hooks/usePage";
-import { useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import { HamburgerButton } from "./hamburger";
 import { LogoComponent } from "./Logo";
 import { NavBarLink } from "./navbar-link";
@@ -9,9 +9,29 @@ import { ThemeToggle } from "./theme-toggle";
 
 export const NavBar = () => {
 	const [open, setOpen] = useState(false);
+	const navRef = useRef<HTMLDivElement>(null);
+
+	const handleClose = useCallback(() => {
+		setOpen(false);
+	}, []);
+
+	useEffect(() => {
+		const handleClickOutside = (event: Event) => {
+			if (
+				navRef.current &&
+				!navRef.current?.contains(event.target as Node)
+			) {
+				handleClose();
+			}
+		};
+		document.addEventListener("click", handleClickOutside, true);
+	}, [navRef]);
 
 	return (
-		<nav className="flex flex-wrap items-center justify-between p-3">
+		<nav
+			ref={navRef}
+			className="flex flex-wrap items-center justify-between py-3 px-10"
+		>
 			<LogoComponent />
 			<div className="flex md:hidden">
 				<HamburgerButton open={open} setOpen={setOpen} />
@@ -22,14 +42,21 @@ export const NavBar = () => {
 				} w-full md:w-auto md:flex md:flex-row gap-4 items-center md:mt-0 md:border-none`}
 			>
 				{pages.map((p) => (
-					<NavBarLink key={p.href} href={p.href} label={p.label} />
+					<NavBarLink
+						key={p.href}
+						href={p.href}
+						label={p.label}
+						callback={handleClose}
+					/>
 				))}
 			</div>
 
 			<div
 				className={`${
-					open ? "block" : "hidden"
-				} w-full text-end md:flex md:w-auto px-2 py-2 md:rounded`}
+					open
+						? "flex justify-center mt-5 py-4 gap-2 border-t-2 border-gray-500"
+						: "hidden"
+				} w-full md:flex md:w-auto`}
 			>
 				<ThemeToggle />
 			</div>
